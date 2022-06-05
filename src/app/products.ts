191b4a9fddec151436a9d95712ccaf35b30e231c
ErikoAndo <andou@digital-knowledge.co.jp>
2022-06-05 13:06:10 +0900

    angular frontend

---
+++ src/app/products.ts
@@ -0,0 +1,82 @@
+export const products = [
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone XL',
+        price: 700,
+        description: 'A large phone with one of the best screens',
+        headingitem: [
+            {
+                heading: 'Phone XL Heading1',
+                headingtext: 'Phone XL Details1'
+            },
+            {
+                heading: 'Phone XL Heading2',
+                headingtext: 'Phone XL Details2'
+            },
+            {
+                heading: 'Phone XL Heading3',
+                headingtext: 'Phone XL Heading3'
+            }
+        ]
+    },
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Mini',
+        price: 600,
+        description: 'A great phone with one of the best cameras',
+        headingitem: [
+            {
+                heading: 'Phone Mini Heading1',
+                headingtext: 'Phone Mini Details1'
+            },
+            {
+                heading: 'Phone Mini Heading2',
+                headingtext: 'Phone Mini Details2'
+            },
+            {
+                heading: 'Phone Mini Heading3',
+                headingtext: 'Phone Mini Heading3'
+            }
+        ]
+    },
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Standard',
+        price: 300,
+        description: '',
+        headingitem: [
+            {
+                heading: 'Phone Standard Heading1',
+                headingtext: 'Phone Standard Details1'
+            },
+            {
+                heading: 'Phone Standard Heading2',
+                headingtext: 'Phone Standard Details2'
+            },
+            {
+                heading: 'Phone Standard Heading3',
+                headingtext: 'Phone Standard Heading3'
+            }
+        ]
+    },
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Special',
+        price: 900,
+        description: '',
+        headingitem: [
+            {
+                heading: 'Phone Special Heading1',
+                headingtext: 'Phone Special Details1'
+            },
+            {
+                heading: 'Phone Special Heading2',
+                headingtext: 'Phone Special Details2'
+            },
+            {
+                heading: 'Phone Special Heading3',
+                headingtext: 'Phone Special Heading3'
+            }
+        ]
+    }
+]
